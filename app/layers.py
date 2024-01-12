9f282a46d9fd1c06c079e0eba8c2d081ad371208
Meirbek-dev <meirbek.dev@pm.me>
2024-01-12 22:53:02 +0600

    removed concat layer

--- app/layers.py
+++ app/layers.py
@@ -5,21 +5,6 @@ import tensorflow as tf
 from tensorflow.keras.layers import Layer
 
 
-@tf.keras.utils.register_keras_serializable(name="ConcatLayer")
-class ConcatLayer(Layer):
-    def __init__(self, axis=0, **kwargs):
-        super().__init__(**kwargs)
-        self.axis = axis
-
-    def call(self, inputs):
-        return tf.concat(inputs, axis=self.axis)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({"axis": self.axis})
-        return config
-
-
 # Сиамский класс дистанции L1
 class L1Dist(Layer):
     def __init__(self, **kwargs):
